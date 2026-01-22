cf0a981b9e99d1ba81bf8fa923136a8478a0f794
EnekoADC <enekoadc@proton.me>
2026-01-22 16:41:26 +0000

    Implementacion de la version sencilla de la tabla de conexiones

---
+++ Desarrollo/modules/tcpTable.h
@@ -0,0 +1,46 @@
+#ifndef EAD_TCP_TABLE
+#define EAD_TCP_TABLE
+
+#include <stdint.h>
+
+#define MAX_CONN 1<<3
+
+struct five_tuple
+{
+    uint32_t src_ip;
+    uint32_t dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint8_t proto;
+};
+
+struct tcp_flow
+{
+    //ID de la conexión
+    struct five_tuple id;
+
+    //Timestamps
+    uint64_t first_seen;
+    uint64_t last_seen;
+
+    //Flags de estado
+    uint8_t tcp_state;
+
+    //Estadísticas
+    unsigned int n_bytes;
+    unsigned int n_packets;
+};
+
+struct conn_table
+{
+    tcp_flow connections[MAX_CONN];
+
+    unsigned int next_elem;
+
+    tcp_flow* first;
+    tcp_flow* last;
+};
+
+conn_table* initTcpTable();
+
+#endif
